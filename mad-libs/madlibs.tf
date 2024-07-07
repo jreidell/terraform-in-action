cbd6e92ce11f8a33366e098c6bb6e40489100238
Joe Reidell <joe@reidell.net>
2024-07-07 17:58:03 -0400

    added more templates and additional logic to the madlib generator.

--- mad-libs/madlibs.tf
+++ mad-libs/madlibs.tf
@@ -5,6 +5,14 @@ terraform {
       source = "hashicorp/random"
       version = "~> 3.0"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.1"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -45,33 +53,62 @@ variable "words" {
 
 }
 
+variable "num_files" {
+  description = "The number of files to generate"
+  type = number
+  default = 100
+}
+
+locals {
+  uppercase_words = {for k, v in var.words: k => [for s in v : upper(s)]}
+}
+
 resource "random_shuffle" "random_nouns" {
-  input = var.words["nouns"]
+  count = var.num_files
+  input = local.uppercase_words["nouns"]
 }
 
 resource "random_shuffle" "random_adjectives" {
-  input = var.words["adjectives"]
+  count = var.num_files
+  input = local.uppercase_words["adjectives"]
 }
 
 resource "random_shuffle" "random_verbs" {
-  input = var.words["verbs"]
+  count = var.num_files
+  input = local.uppercase_words["verbs"]
 }
 
 resource "random_shuffle" "random_adverbs" {
-  input = var.words["adverbs"]
+  count = var.num_files
+  input = local.uppercase_words["adverbs"]
 }
 
 resource "random_shuffle" "random_numbers" {
-  input = var.words["numbers"]
+  count = var.num_files
+  input = local.uppercase_words["numbers"]
 }
 
-output "mad_libs" {
-  value = templatefile("${path.module}/templates/alice.txt", 
+locals {
+  templates = tolist(fileset(path.module, "templates/*.txt"))
+}
+
+resource "local_file" "mad_libs" {
+  count = var.num_files
+  filename = "output/madlibs-${count.index}.txt"
+  content = templatefile(element(local.templates, count.index), 
   {
-    nouns = random_shuffle.random_nouns.result
-    adjectives = random_shuffle.random_adjectives.result
-    verbs = random_shuffle.random_verbs.result
-    adverbs = random_shuffle.random_adverbs.result
-    numbers = random_shuffle.random_numbers.result
+    nouns = random_shuffle.random_nouns[count.index].result
+    adjectives = random_shuffle.random_adjectives[count.index].result
+    verbs = random_shuffle.random_verbs[count.index].result
+    adverbs = random_shuffle.random_adverbs[count.index].result
+    numbers = random_shuffle.random_numbers[count.index].result
   })
 }
+
+data "archive_file" "mad_libs" {
+  depends_on  = [local_file.mad_libs]
+  type        = "zip"
+  source_dir  = "${path.module}/output"
+  output_path = "${path.module}/output/madlibs.zip"
+}
+
--- mad-libs/terraform.tfvars
+++ mad-libs/terraform.tfvars
@@ -1,5 +1,5 @@
 words = {
-  nouns = [ "army", "panther", "walnets", "sandwich", "Zues", "banana", "cat", "jellyfish", "jigsaw", "violin", "milk", "sun", "Sam", "star", "planet", "galaxy", "street", "blackhole", "wormhole", "asteroid", "comet", "meteor", "nebula", "quasar" ]
+  nouns = [ "army", "panther", "walnuts", "sandwich", "Zues", "banana", "cat", "jellyfish", "jigsaw", "violin", "milk", "sun", "Sam", "star", "planet", "galaxy", "street", "blackhole", "wormhole", "asteroid", "comet", "meteor", "nebula", "quasar" ]
   adjectives = [ "bitter", "sticky", "thundering", "abundant", "chubby", "grumpy", "silly", "fancy", "fuzzy", "giant", "itchy", "jolly", "lazy", "magnificent", "nervous", "obnoxious", "pretty", "quirky", "ruthless", "sassy", "tasty", "ugly", "vivacious", "witty", "zany" ]
   verbs = [ "run", "dance", "love", "respect", "kicked", "baked", "slept", "sang", "drew", "flew", "grew", "hurt", "joked", "knit", "leapt", "melted", "napped", "painted", "quizzed", "raced", "sailed", "talked", "united", "voted", "whispered" ]
   adverbs = [ "delicately", "beautifully", "quickly", "truthfully", "wearily", "happily", "sadly", "angrily", "loudly", "softly", "briskly", "calmly", "eagerly", "fiercely", "gently", "hungrily", "innocently", "jovially", "kindly", "lazily", "merrily", "nervously", "obediently", "politely", "quietly" ]
